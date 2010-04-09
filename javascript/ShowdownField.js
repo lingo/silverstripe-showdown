fd137a22de45fa7fedf4e2a680763b6f1a9acd2b
Luke Hudson <lukeletters@gmail.com>
2010-04-09 16:07:54 +1200

    MINOR: Limit updates to UPDATE_INTERVAL.  Better for large pages

--- javascript/ShowdownField.js
+++ javascript/ShowdownField.js
@@ -9,15 +9,22 @@ Behaviour.register({
 			(function($){
 				 // build the converter
 				 var converter = new Showdown.converter();
+				 var UPDATE_INTERVAL = 300; // msec
 
-				 console.log($('.showdown textarea'));
+				 var timerID = 0;
 				 var previewFunction = function(e, elt) {
-					 var prev = $(elt).siblings('div.preview');
-					 console.log(e, prev, converter);
-					 var text = elt.value;
-					 text = converter.makeHtml(text);
-					 console.log(prev, text);
-					 prev.html(text);
+					 if (timerID)
+					 	clearTimeout(timerID);
+				 	 timerID = setTimeout(function() {
+						 //var prev = $(elt).siblings('iframe.preview');
+						 var prev = $('div.preview', $(elt).parents('div.showdown'));
+						 // console.log(e, 'prev=', prev, 'conv=',converter);
+						 var text = elt.value;
+						 text = converter.makeHtml(text);
+						 // console.log('prev=', prev, 'text=', text);
+						 // prev = $(prev[0].contentDocument.body);
+						 prev.html(text);
+					 }, UPDATE_INTERVAL);
 				 }
 				 $('.showdown textarea').keyup(function(e) { return previewFunction(e, this); });
 			 })(jQuery);
